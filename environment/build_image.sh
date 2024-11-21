111947235b56de40de66c84873c20b501953fc1d
Ivan Ayala <ivan.ayala.alcalde@gmail.com>
2024-11-21 17:34:09 +0000

    Minor fixes

--- environment/build_image.sh
+++ environment/build_image.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
 run () {
+	export DOCKER_BUILDKIT=1
 	local current_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 	pushd $current_dir > /dev/null
 	buildkit_progress=plain docker build \
 	  -f Dockerfile.dev \
+	  --secret id=my_ssh_key,src=/home/iayala/.ssh/id_rsa \
 	  -t wmf_scraper_front:1.0.1 \
 	  --progress=plain \
 	  ..
